0af035ed1c662b3495590c1b4cf215547e70aef7
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-08-24 19:59:06 -0400

    Make JPA entity

--- game-backend/src/main/java/com/guessinggame/game_backend/Player.java
+++ game-backend/src/main/java/com/guessinggame/game_backend/Player.java
@@ -1,12 +1,22 @@
 package com.guessinggame.game_backend;
 
+import java.util.UUID;
+
+@Entity
 public class Player {
     private String username;
     private String socketId;
+    @ID
+    @GeneratedValue
+    private UUID uuid;
+
+    protected Player() {
+    }
 
     public Player(String username, String socketId) {
         this.username = username;
         this.socketId = socketId;
+        this.uuid = UUID.randomUUID();
     }
 
     public String getUsername() {
@@ -16,4 +26,9 @@ public class Player {
     public String getSocketId() {
         return this.socketId;
     }
+
+    public UUID getUUID() {
+        return this.uuid;
+    }
+
 }

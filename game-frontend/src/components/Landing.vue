f582d0e10f859512f361354a7895b1589343d9e1
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-08-24 20:00:24 -0400

    Create Landing page component

---
+++ game-frontend/src/components/Landing.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { ref } from 'vue'
+    console.log(crypto.randomUUID())
+
+
+    var username = ref('')
+    async function send_username(username){
+        const response = await fetch("http://localhost:8080/game-backend", {
+            method: "POST",
+            body: JSON.stringify({ username: { username } }),
+            
+});
+    }
+
+</script>
+
+<template>
+
+    <p>Enter a username</p>
+    <input type="text" @click = send_username(username) >{{ username }}</input>
+    <button type="submit">Submit</button>
+
+</template>
+
+<style></style>
